a8fcb8f77173c25b46167dc8683c71b43133bbd5
KouseiImai <kouseiimai16340@gmail.com>
2020-09-28 23:20:32 +0900

    ActiveHash、categoryの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,17 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    Product.create(product_params)
+  end
+
   private
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def product_params
+    params.require(:product).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  has_one_attached :image
+
+
   #バリデーション
+  validates :category_id, numericality: { other_than:1 }
   with_options presence: true do
     validates :neme
     validates :explanation
