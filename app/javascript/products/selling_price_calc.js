56aea2c4d740aa0702a30c761b7851568075dc11
KouseiImai <kouseiimai16340@gmail.com>
2020-10-06 18:40:38 +0900

    編集画面のフォーム部分変更・初期値設定

--- app/javascript/products/selling_price_calc.js
+++ app/javascript/products/selling_price_calc.js
@@ -1,4 +1,15 @@
 window.addEventListener('load', function(){
+  let price_input1 = document.getElementById("item-price").value
+  let addTaxPrice1 = document.getElementById("add-tax-price")
+  let profit1 = document.getElementById("profit")
+
+  let fee1 =  price_input1 * 0.1
+      fee1 = Math.floor(fee1)
+  let sales_profit1 = price_input1 - fee1
+      sales_profit1 = Math.ceil(sales_profit1)
+      addTaxPrice1.innerHTML = fee1
+      profit1.innerHTML = sales_profit1
+  
   document.addEventListener('input', function(){
     let price_input = document.getElementById("item-price").value
     let addTaxPrice = document.getElementById("add-tax-price")
