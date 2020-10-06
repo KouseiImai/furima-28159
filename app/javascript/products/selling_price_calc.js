219c91365c89b8a253b1455b4d4b3fa6b8abc9b3
KouseiImai <kouseiimai16340@gmail.com>
2020-10-06 20:55:01 +0900

    コードレビュー内容の修正2

--- app/javascript/products/selling_price_calc.js
+++ app/javascript/products/selling_price_calc.js
@@ -1,15 +1,16 @@
 window.addEventListener('load', function(){
-  let price_input1 = document.getElementById("item-price").value
-  let addTaxPrice1 = document.getElementById("add-tax-price")
-  let profit1 = document.getElementById("profit")
+  let price_input = document.getElementById("item-price").value
+  let addTaxPrice = document.getElementById("add-tax-price")
+  let profit = document.getElementById("profit")
 
-  let fee1 =  price_input1 * 0.1
-      fee1 = Math.floor(fee1)
-  let sales_profit1 = price_input1 - fee1
-      sales_profit1 = Math.ceil(sales_profit1)
-      addTaxPrice1.innerHTML = fee1
-      profit1.innerHTML = sales_profit1
-  
+  let fee =  price_input * 0.1
+      fee = Math.floor(fee)
+  let sales_profit = price_input - fee
+      sales_profit = Math.ceil(sales_profit)
+
+  addTaxPrice.innerHTML = fee
+  profit.innerHTML = sales_profit
+
   document.addEventListener('input', function(){
     let price_input = document.getElementById("item-price").value
     let addTaxPrice = document.getElementById("add-tax-price")
