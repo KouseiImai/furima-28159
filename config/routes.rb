12346de21643c835346f1b3da94de6b095ca619c
KouseiImai <kouseiimai16340@gmail.com>
2020-10-12 23:46:29 +0900

    ルーティングでネストを使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root to:'products#index'
-  resources :products
-  resources :purchases, only: :index
+  resources :products do
+    resources :purchases, only: :index
+  end
   devise_for :users
   resources :users, only: :show
 end
