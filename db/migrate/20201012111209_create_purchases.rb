5fde9061063a0fec7d427c506b04a29cf25057c3
KouseiImai <kouseiimai16340@gmail.com>
2020-10-12 20:27:34 +0900

    purchasesテーブルの作成

--- db/migrate/20201012111209_create_purchases.rb
+++ db/migrate/20201012111209_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.integer :product_id, null: false, foreign_key: true
+      t.integer :user_id,    null: false, foreign_ley: true
       t.timestamps
     end
   end
