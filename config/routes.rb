61f7957b58b8cf251e790929e70928c95de7d827
KouseiImai <kouseiimai16340@gmail.com>
2020-10-12 20:45:24 +0900

    purchaseコントローラーの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to:'products#index'
   resources :products
-
+  resources :purchases, only: :index
   devise_for :users
   resources :users, only: :show
 end
