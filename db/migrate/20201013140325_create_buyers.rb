1b6522fda77d68f534193cdde8f157a560168846
KouseiImai <kouseiimai16340@gmail.com>
2020-10-13 23:39:46 +0900

    buyer(発送先情報登録用)テーブルの作成

---
+++ db/migrate/20201013140325_create_buyers.rb
@@ -0,0 +1,14 @@
+class CreateBuyers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buyers do |t|
+      t.integer :purchase_id,     null: false, foreign_key: true
+      t.string :postal_code,      null: false
+      t.integer :prefecture_id,   null: false
+      t.string :municipal,        null: false
+      t.string :address,          null: false
+      t.string :building_name
+      t.string :phone_number,     null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_111209) do
+ActiveRecord::Schema.define(version: 2020_10_13_140325) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2020_10_12_111209) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "purchase_id", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipal", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
